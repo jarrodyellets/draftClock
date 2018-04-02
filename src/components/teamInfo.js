9341ce5ad519de1b89f6c0559860f551f24fb7d1
Jarrod Yellets <buddyfeagles@jarrod-yelletss-macbook-pro.home>
2018-04-02 13:09:39 +0200

    refactor teams comp

--- src/components/teamInfo.js
+++ src/components/teamInfo.js
@@ -1,30 +1,21 @@
-import React, {Component} from 'react';
+import React from 'react';
 
-class Team extends Component {
-	constructor(props) {
-		super(props);
-
-
-	}
-
-
-	render() {
-		let currentTeam = this.props.team
-		let currentTeamObj = this.props.teams.filter(function(team) {
-			return team.name == currentTeam;
-		});
-		return (
-			<div>
-				<div className="teamIcon">
-					<img className="logo" src={currentTeamObj[0].logo} />
-					<div className="roundInfo">
-						<div className="round">ROUND: <span className="num">{Math.ceil(this.props.pick / 12)}</span></div>
-						<div className="pick">PICK: <span className="num">{this.props.pick}</span></div>
-					</div>
+const Team = (props) => {
+	let currentTeam = props.team
+	let currentTeamObj = props.teams.filter(function(team) {
+		return team.name == currentTeam;
+	});
+	return (
+		<div>
+			<div className="teamIcon">
+				<img className="logo" src={currentTeamObj[0].logo} />
+				<div className="roundInfo">
+					<div className="round">ROUND: <span className="num">{Math.ceil(props.pick / 12)}</span></div>
+					<div className="pick">PICK: <span className="num">{props.pick}</span></div>
 				</div>
-			</div>	
-			)
-	}
+			</div>
+		</div>
+	)
 }
 
 export default Team;
